1a287743f6c4412330be9a8c63eacd1c0038d8d7
szafar-7101 <sa.zafar7101@gmail.com>
2026-03-14 17:09:22 +0530

    Constants setup

---
+++ src/smartgrid/constants/constants.py
@@ -0,0 +1,13 @@
+
+# Constants used throughout the Smart Grid project
+CONFIG_FILE_PATH = "configs/config.yaml"
+PARAMS_FILE_PATH = "configs/params.yaml"
+SCHEMA_FILE_PATH = "configs/schema.yaml"
+
+ARTIFACTS_DIR = "artifacts" # This is where all generated files will be stored 
+
+
+# Will be used in pipelines later, remove comment after implementation
+MODEL_FILE_NAME = "model.pkl"
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
--- test_exception.py
+++
@@ -1,8 +0,0 @@
-import sys
-from smartgrid.exception.exception import SmartGridException
-
-
-try:
-    a = 10 / 0
-except Exception as e:
-    raise SmartGridException(e, sys)
